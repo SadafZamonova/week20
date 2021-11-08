b4b78fc23a9a1b6ee07c44179e538fe8f59cf6e7
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-11-08 21:09:56 +0500

    2

--- first/index.js
+++ first/index.js
@@ -2,19 +2,23 @@ let json = `[{
     "person": "Супермен",
     "universe":  "DC Comics",
     "activity": "борец за справедливость",
-    "superpowers": "непробиваемость, суперсила, полет, самоисцеление, суперзрение и суперслух, классный костюм"
+    "superpowers": "непробиваемость, суперсила, полет, самоисцеление, суперзрение и суперслух, классный костюм",
+    "isRange": "рейтинг"
 },
+
 {
     "person": "Железный человек",
     "universe": "Marvel Comics",
     "activity":  "гений, миллиардер, плейбой, филантроп",
-    "superpowers":  "высокий уровень интеллекта, широкие познания науки и техники, связь со всемирной паутиной, бронекостюмы"
+    "superpowers":  "высокий уровень интеллекта, широкие познания науки и техники, связь со всемирной паутиной, бронекостюмы",
+    "isRange": "рейтинг"
 },
 {
     "person": "Чудо-женщина",
     "universe":  "DC Comics",
     "activity": "супергероиня, секретарь-референт",
-    "superpowers": "сверхчеловеческая сила искорость, выносливость, полет"
+    "superpowers": "сверхчеловеческая сила искорость, выносливость, полет",
+    "isRange": "рейтинг"
 }]`;
 
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -27,8 +31,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
     <h3> Вселенная: ${superhero.universe} </h3>
     <div>Род деятельности: ${superhero.activity} </div>
     <div>Суперсилы: ${superhero.superpowers} </div>
-    </div>`;
+    <input type="range" id="one" max="10"> <span>10</span> ${superhero.isRange}
+   </div>`;
     }
     document.querySelector("#container").innerHTML = superherosContent;
+document.querySelector("#one").oninput = () => {
+    let range = document.querySelector("#one").value;
+    document.querySelector("span").innerHTML=range;
+}
+
+setFromlocalStorage();
 
 });
+
+function getFromlocalStorage(){
+    localStorage.getItem("json")
+
+}
+
+
+function setFromlocalStorage(){
+    localStorage.setItem("json", json);
+}
+
+setFromlocalStorage();
