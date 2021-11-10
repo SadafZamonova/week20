393781aaa8743a0976405fe02124617a9d283b2a
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-11-10 23:01:00 +0500

    ыап

--- first/index.js
+++ first/index.js
@@ -1,28 +1,29 @@
 let json = [{
-    "person": "Супермен",
-    "universe":  "DC Comics",
-    "activity": "борец за справедливость",
-    "superpowers": "непробиваемость, суперсила, полет, самоисцеление, суперзрение и суперслух, классный костюм",
-    "isRange": 7,
-    "id" : "one"
-},
+        "person": "Супермен",
+        "universe": "DC Comics",
+        "activity": "борец за справедливость",
+        "superpowers": "непробиваемость, суперсила, полет, самоисцеление, суперзрение и суперслух, классный костюм",
+        "isRange": 7,
+        "id": "one"
+    },
 
-{
-    "person": "Железный человек",
-    "universe": "Marvel Comics",
-    "activity":  "гений, миллиардер, плейбой, филантроп",
-    "superpowers":  "высокий уровень интеллекта, широкие познания науки и техники, связь со всемирной паутиной, бронекостюмы",
-    "isRange": 3,
-    "id" : "two"
-},
-{
-    "person": "Чудо-женщина",
-    "universe":  "DC Comics",
-    "activity": "супергероиня, секретарь-референт",
-    "superpowers": "сверхчеловеческая сила искорость, выносливость, полет",
-    "isRange": 5,
-    "id" : "three"
-}];
+    {
+        "person": "Железный человек",
+        "universe": "Marvel Comics",
+        "activity": "гений, миллиардер, плейбой, филантроп",
+        "superpowers": "высокий уровень интеллекта, широкие познания науки и техники, связь со всемирной паутиной, бронекостюмы",
+        "isRange": 3,
+        "id": "two"
+    },
+    {
+        "person": "Чудо-женщина",
+        "universe": "DC Comics",
+        "activity": "супергероиня, секретарь-референт",
+        "superpowers": "сверхчеловеческая сила искорость, выносливость, полет",
+        "isRange": 5,
+        "id": "three"
+    }
+];
 
 document.addEventListener("DOMContentLoaded", function (event) {
     let superheros = getFromlocalStorage();
@@ -43,23 +44,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
     <div>Суперсилы: ${superhero.superpowers} </div>
     <input   type="range" id="${superhero.id}" max="10"> <h5>Рейтинг:</h5> <span id="span_${superhero.id}">  ${superhero.isRange}</span> 
    </div>`
-   document.querySelector("#container").innerHTML = superherosContent;
+        document.querySelector("#container").innerHTML = superherosContent;
 
-   document.addEventListener("input", function() {
-    let range = document.querySelector(`#${superhero.id}`).value;
-    document.querySelector(`#span_${superhero.id}`).innerHTML=range;
-    superhero.isRange = range;
-    superheros[i] = superhero;
-    setFromlocalStorage(superheros);
-   });
+        document.addEventListener("input", function () {
+            let range = document.querySelector(`#${superhero.id}`).value;
+            document.querySelector(`#span_${superhero.id}`).innerHTML = range;
+            superhero.isRange = range;
+            superheros[i] = superhero;
+            setFromlocalStorage(superheros);
+        });
     }
 });
 
-function getFromlocalStorage(){
+function getFromlocalStorage() {
     return localStorage.getItem("heroes")
 
 }
 
-function setFromlocalStorage(data){
+function setFromlocalStorage(data) {
     localStorage.setItem("heroes", JSON.stringify(data));
 }
